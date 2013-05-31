75ef206931f2bd0cc32cb6f6a354c94f16b287f2
Alpha-tester <x3r0.mhu@gmail.com>
2013-05-31 10:19:23 +0630

    Added sys argument
    Added the option to use argument.
    -o            for viewing own ip address
    -u <url> for viewing ip of the url

--- ip-tools.py
+++ ip-tools.py
@@ -1,6 +1,3 @@
-
-
-
 #-------------------------------------------------------------------------------
 # Name:        IP tools
 # Purpose:     To make a few tools that assist in networking
@@ -11,17 +8,28 @@
 # Licence:     <Opensource GNU>
 #-------------------------------------------------------------------------------
 
-import urllib2, socket
+import urllib2, socket, sys
 from dns.exception import *
 from time import sleep
+
+def usage():
+    print'''
+    Usage:
+        IP-tools.py -u <url>    you can include http:// and / or not.
+        IP-tools.py -o          View your own ip address
+    '''
 
 def reverse_dnslookup(host):
     '''Get website ip address'''
     try :
+        if '/' in host : host = host.replace('/','')
+        if 'http:' in host : host = host.replace('http:','')
         addr = socket.gethostbyname(host)
-        print'IP address is : ',addr
-        sleep(2)
-        menu()
+        print''
+        print'+'+'='*32 +'+'
+        print'|IP address is : ',addr,'|'
+        print'+'+'='*32 +'+'
+
     except Timeout:
         print'[*] The attempt to connect timed-out'
         if raw_input('Retry? (y) (n)') == 'y':
@@ -33,10 +41,12 @@ def whatismyip():
     '''Find your own ip address by opening a whatismyip website'''
     try:
         ip = urllib2.urlopen('http://whatismyip.akamai.com/', timeout=10).read()
-        print'\nYour ip address is ',ip
+        print''
+        print'+'+'='*33 +'+'
+        print'|Your ip address is ',ip,'|'
+        print'+'+'='*33 +'+'
         print'\n'
         sleep(2)
-        menu()
 
     except Timeout:
         print'[*] The attempt to connect timed-out'
@@ -54,12 +64,31 @@ def menu():
     choice = raw_input('\n : ')
     if choice == '1':
         hostname = raw_input('\nEnter host name : ')
+        if hostname.endswith('/'):
+            hostname = hostname.replace('/','')
+            if hostname.startswith('http:'): hostname = hostname.replace('http:','')
+        elif hostname.startswith('http:'):
+            hostname = hostname.replace('http:','')
+            if hostname.endswith('/'):
+                hostname = hostname.replace('/','')
+        else:
+            pass
         reverse_dnslookup(hostname)
     elif choice == '2':
         whatismyip()
+        sleep(2)
+        menu()
     elif choice == '0':
         exit()
 
 if __name__ == '__main__':
-    menu()
-
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-u':
+            host = sys.argv[2]
+            reverse_dnslookup(host)
+        elif sys.argv[1] == '-o':
+            whatismyip()
+        else:
+            usage()
+    else:
+        menu()
