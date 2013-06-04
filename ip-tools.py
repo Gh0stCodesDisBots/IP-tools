1fa07b081a9f78102518500b62f1c1e14c012d0e
Alpha Tester <x3r0.mhu@gmail.com>
2013-06-04 16:33:59 +0630

    Added port scan feature!
    Added a new feature... <Port Scan>
    Syntax:
    
    ip-tools.py -p 127.0.0.1
    ip-tools.py -p 127.0.0.1 5-200
    ip-tools.py -p

--- ip-tools.py
+++ ip-tools.py
@@ -8,16 +8,33 @@
 # Licence:     <Opensource GNU>
 #-------------------------------------------------------------------------------
 
-import urllib2, socket, sys
+import urllib2, socket, sys, random
 from dns.exception import *
 from time import sleep
+from sys import stdout
+from time import sleep
+
+
+def output(text):
+    for char in text:
+        stdout.write(char)
+        stdout.flush()
+        t = [0.01,0.02]
+        time = random.choice(t)
+        sleep(time)
 
 def usage():
-    print'''
+    output('''
     Usage:
-        IP-tools.py -u <url>    you can include http:// and / or not.
-        IP-tools.py -o          View your own ip address
-    '''
+        IP-tools.py -u <url>    -you can include http:// and / or not.
+        IP-tools.py -o          -View your own ip address
+        IP-tools.py -p <ip> <portrange>
+        IP-tools.py -p          -Go to portscanner directly
+                                portrange can be specified or not.
+        If you wish to specify.. It need to be in this format
+
+        IP-tools.py -p 127.0.0.1 10-20
+    ''')
 
 def reverse_dnslookup(host):
     '''Get website ip address'''
@@ -42,9 +59,10 @@ def whatismyip():
     try:
         ip = urllib2.urlopen('http://whatismyip.akamai.com/', timeout=10).read()
         print''
-        print'+'+'='*33 +'+'
-        print'|Your ip address is ',ip,'|'
-        print'+'+'='*33 +'+'
+        banner1 = '+'+'='*33 +'+'
+        output(banner1)
+        print'\n|Your ip address is ',ip,'|\n'
+        output(banner1)
         print'\n'
         sleep(2)
 
@@ -56,14 +74,19 @@ def whatismyip():
             menu()
 
 def menu():
-    print'\n\n\t','='*30
-    print'\t1. Find ip of a website'
-    print'\t2. Find your own ip address'
-    print'\n\t0. Exit'
-    print'\t','='*30
+    banner = '\n\n\t','='*30
+    output(banner)
+    output('\n\t| 1. Find ip of a website')
+    output('\n\t| 2. Find your own ip address')
+    output('\n\t| 3. Port Scanner')
+    output('\n\n\t| 0. Exit')
+    output(banner)
+
+
     choice = raw_input('\n : ')
     if choice == '1':
-        hostname = raw_input('\nEnter host name : ')
+        ouput('\nEnter host name')
+        hostname = raw_input(" : ")
         if hostname.endswith('/'):
             hostname = hostname.replace('/','')
             if hostname.startswith('http:'): hostname = hostname.replace('http:','')
@@ -78,17 +101,80 @@ def menu():
         whatismyip()
         sleep(2)
         menu()
+    elif choice == '3':
+        portrange = ''
+        ip = ''
+        port_scan(ip,portrange)
     elif choice == '0':
         exit()
 
+def port_scan(ip,portrange):
+    try:
+        if ip == '':
+            output('Enter target ip address')
+            ip = raw_input(' : ')
+        if portrange == '' :
+            output("Enter port range eg(5-200)")
+            lowport,highport = raw_input(" : ").split('-')
+        else:
+    	  lowport,highport = portrange.split('-')
+        msg = '[~] Scanning host ', ip,' from port ', lowport,' to port ',highport,' [~]\n'
+        output(msg)
+
+        for port in range(eval(lowport),eval(highport)+1):
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            status = s.connect_ex((ip,port))
+            if (status ==0):
+                opn = '\n========[1] Port '+str(port)+' -OPEN   [1]========'
+                output(opn)
+            else:
+                cld = '\n\t[0] Port '+str(port)+' -CLOSED [0]'
+                output(cld)
+            s.close()
+        output('\n\nGreetingz from <4sec>\n')
+
+    except KeyboardInterrupt:
+        output('\n\t[!] User interrupt')
+        output('\n\t[!] Program terminated ')
+        exit()
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         if sys.argv[1] == '-u':
             host = sys.argv[2]
             reverse_dnslookup(host)
         elif sys.argv[1] == '-o':
             whatismyip()
+        elif sys.argv[1] == '-p':
+            if len(sys.argv) == 3 :
+                ip = sys.argv[2]
+                portrange = sys.argv[3]
+                port_scan(ip,portrange)
+            elif len(sys.argv) <= 2:
+                ip = sys.argv[2]
+                port = ''
+                port_scan(ip,port)
+            else:
+                ip = ''
+                portrange = ''
+                port_scan(ip,portrange)
         else:
             usage()
+    elif len(sys.argv) == 2:
+        ip = ''
+        portrange = ''
+        port_scan(ip,portrange)
+
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == '-p':
+            ip = sys.argv[2]
+            portrange = ''
+            port_scan(ip,portrange)
+
+    elif len(sys.argv) == 4:
+        if sys.argv[1] == '-p':
+            ip = sys.argv[2]
+            portrange = sys.argv[3]
+            port_scan(ip,portrange)
     else:
         menu()
