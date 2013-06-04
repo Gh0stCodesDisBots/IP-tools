68acff9adcf4f41a1b5f595322d69716947d5155
Alpha Tester <x3r0.mhu@gmail.com>
2013-06-04 16:46:07 +0630

    Updated the help message.

--- ip-tools.py
+++ ip-tools.py
@@ -26,14 +26,19 @@ def output(text):
 def usage():
     output('''
     Usage:
-        IP-tools.py -u <url>    -you can include http:// and / or not.
-        IP-tools.py -o          -View your own ip address
-        IP-tools.py -p <ip> <portrange>
-        IP-tools.py -p          -Go to portscanner directly
-                                portrange can be specified or not.
-        If you wish to specify.. It need to be in this format
-
-        IP-tools.py -p 127.0.0.1 10-20
+        IP-tools.py -o                   -View your own ip address
+        IP-tools.py -u <url>             -View ip address of a website
+        IP-tools.py                      -Go to program menu
+        IP-tools.py -p                   -Go to portscan section
+        IP-tools.py -p <ip>              -Scan that ip for open ports. 
+                                          You can specify the port range too
+        IP-tools.py -p <ip> <port-range> -Scan that ip for open ports from 5 to 100.
+        
+    Example:
+        IP-tools.py -u http://www.google.com/
+        IP-tools.py -u www.google.com
+        IP-tools.py -p 127.0.0.1
+        IP-tools.py -p 127.0.0.1 5-100
     ''')
 
 def reverse_dnslookup(host):
